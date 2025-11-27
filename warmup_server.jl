c93567d41217f9e5e647fa0dd7a92e4a90e9d315
Tamme Claus <tamme.claus@rwth-aachen.de>
2025-11-27 13:38:12 +0100

    also warmup notebook in postBuild

--- warmup_server.jl
+++ warmup_server.jl
@@ -8,10 +8,15 @@ import Pluto
 @info("starting new notebook from setup.py")
 sesh = Pluto.ServerSession(options=Pluto.Configuration.from_flat_kwargs(; include("pluto_server_config.jl")...))
 nb = Pluto.SessionActions.new(sesh; run_async=false)
-
-
 @info("shutting down notebook from setup.py")
 Pluto.SessionActions.shutdown(sesh, nb; async=false)
+
+
+notebook_url = "https://raw.githubusercontent.com/tam724/test_pluto_binder/refs/heads/main/notebook.jl"
+@info("starting notebook $(notebook_url) from setup.py")
+nb2 = Pluto.SessionActions.open_url(sesh, notebook_url; run_async=false)
+@info("shutting down notebook from setup.py")
+Pluto.SessionActions.shutdown(sesh, nb2; async=false)
 
 # @info("$(versioninfo())")
 
